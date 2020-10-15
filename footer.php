ddb8f5e8a0fba7d3f3ea04aea4a4f826c73c51e9
Ricardo <riicardolivera@gmail.com>
2020-10-15 17:58:58 -0400

    refactor(footer) correção e add custom fields

--- footer.php
+++ footer.php
@@ -2,49 +2,49 @@
 
 	<style type="text/css">
 		.quebra {
-		background: url("<?php the_field('background_quebra'); ?>") no-repeat center;
+		background: url("<?php the_field('background_footer', $contato); ?>") no-repeat center;
 		background-size: cover;
 		}
 	</style>
 
 	<div class="quebra">
 		<blockquote class="quote-externo container">
-		<p>“o verdadeiro segredo da felicidade está em ter um genuíno interesse por todos os detalhes da vida cotidiana.”</p>
-		<cite>WILLIAM MORRIS</cite>
+		<?php the_field('frase_footer', $contato); ?>
+		<cite><?php the_field('autor_footer', $contato); ?></cite>
 		</blockquote>
 	</div>
 
 <footer>
-			<div class="footer">
-				<div class="container">
+	<div class="footer">
+		<div class="container">
 
-					<div class="grid-8 footer_historia">
-						<h3>Nossa História</h3>
-						<?php the_field('resumo_historia', $contato); ?>
-					</div>
-
-					<div class="grid-4 footer_contato">
-						<h3>Contato</h3>
-						<ul>
-							<li>- <?php the_field('telefone', $contato); ?></li>
-							<li>- <?php the_field('email', $contato); ?></li>
-							<li>- <?php the_field('endereco1', $contato); ?></li>
-						</ul>
-					</div>
-
-					<div class="grid-4 footer_redes">
-						<h3>Redes Socias</h3>
-						<?php include(TEMPLATEPATH . "/inc/redes-sociais.php"); ?>
-					</div>
-
-				</div>
+			<div class="grid-8 footer_historia">
+				<h3>Nossa História</h3>
+				<?php the_field('resumo_historia', $contato); ?>
 			</div>
 
-			<div class="copy">
-				<div class="container">
-					<p class="grid-16"><?php bloginfo('name'); ?> <?php echo date("Y"); ?> - Alguns direitos reservados.</p>
-				</div>
+			<div class="grid-4 footer_contato">
+				<h3>Contato</h3>
+				<ul>
+					<li>- <?php the_field('telefone', $contato); ?></li>
+					<li>- <?php the_field('email', $contato); ?></li>
+					<li>- <?php the_field('endereco1', $contato); ?></li>
+				</ul>
 			</div>
+
+			<div class="grid-4 footer_redes">
+				<h3>Redes Socias</h3>
+				<?php include(TEMPLATEPATH . "/inc/redes-sociais.php"); ?>
+			</div>
+
+		</div>
+	</div>
+
+	<div class="copy">
+		<div class="container">
+			<p class="grid-16"><?php bloginfo('name'); ?> <?php echo date("Y"); ?> - Alguns direitos reservados.</p>
+		</div>
+	</div>
 
     <!-- wp_head -->
     <?php wp_footer(); ?>
