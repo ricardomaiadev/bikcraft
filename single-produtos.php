650c47c91d8fb951f09745c2a318efbd6222422a
Ricardo <riicardolivera@gmail.com>
2020-10-16 17:42:46 -0400

    refactor(single-produtos) add custom fields

--- single-produtos.php
+++ single-produtos.php
@@ -1,23 +1,19 @@
 <?php
-	// Template Name: Produtos
+	// Template Name: Single Produtos
 ?>
 <?php get_header(); ?>
+
 	<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+
 		<section class="container produto_item animar-interno">
 			<div class="grid-11">
-				<img src="img/produtos/bikcraft-passeio-1.jpg" alt="Bikcraft Passeio">
-				<h2>Passeio</h2>
+				<img src="<?php the_field('foto_produto1'); ?>" alt="Bikcraft <?php the_title(); ?>">
+				<h2><?php the_title(); ?></h2>
 			</div>
-			<div class="grid-5 produto_icone"><img src="img/produtos/passeio.png" alt="Icone Passeio"></div>
-			<div class="grid-8"><img src="img/produtos/bikcraft-passeio-2.jpg" alt="Bikcraft Passeio"></div>
+			<div class="grid-5 produto_icone"><img src="<?php the_field('icone_produto'); ?>" alt="Icone <?php the_title(); ?>"></div>
+			<div class="grid-8"><img src="<?php the_field('foto_produto2'); ?>" alt="Bikcraft <?php the_title(); ?>"></div>
 			<div class="grid-8 produto_info">
-				<p>Muito melhor do que passear pela orla a vidros fechados. A Bikcraft Passeio é uma bicicleta que une conforto e praticidade para o seu dia a dia. Você nunca mais vai querer saber de outra.</p>
-				<ul>
-					<li>Conforto</li>
-					<li>Praticidade</li>
-					<li>Design</li>
-					<li>Versatilidade</li>
-				</ul>
+				<?php the_content(); ?>
 			</div>
 		</section>
 
@@ -42,20 +38,7 @@
 
 					<button id="enviar" name="enviar" type="submit" class="btn">Enviar</button>
 				</form>
-				<div class="orcamento_dados grid-8">
-					<h3>Dados</h3>
-					<span>+55 21 9999-9999</span>
-					<span>orcamento@bikcraft.com</span>
-					<h3>Monte a sua Bikcraft</h3>
-					<p>Escolha as especificações:</p>
-					<ul>
-						<li>- Cores</li>
-    				<li>- Estilo</li>
-    				<li>- Medidas</li>
-   					<li>- Acessórios</li>
-    				<li>- E Outros</li>
-					</ul>
-				</div>
+				<?php include(TEMPLATEPATH . "/inc/produtos-orcamento.php"); ?>
 			</div>
 		</section>
 	<?php endwhile; else: endif; ?>
