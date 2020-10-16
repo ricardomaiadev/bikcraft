363b923ed9efd0bc0bd6f96e0b2bf65eb02bf1e4
Ricardo <riicardolivera@gmail.com>
2020-10-16 17:39:53 -0400

    refactor(footer) add get_template

--- footer.php
+++ footer.php
@@ -55,8 +55,8 @@
 	<!-- JavaScript -->
 	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
 	<script>window.jQuery || document.write('<script src="js/libs/jquery-1.11.2.min.js"><\/script>')</script>
-	<script src="js/plugins.js"></script>
-	<script src="js/main.js"></script>
+	<script src="<?php echo get_template_directory_uri(); ?>/js/plugins.js"></script>
+	<script src="<?php echo get_template_directory_uri(); ?>/js/main.js"></script>
 	<!-- JavaScript -->
 
 	<script>
